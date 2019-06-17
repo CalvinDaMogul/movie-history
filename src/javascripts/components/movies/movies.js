c5976da2846ae7ba971e77cc61f408179eb9d4db
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-17 18:09:29 -0500

    made changes to watch list

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -7,10 +7,10 @@ import util from '../../helpers/util';
 const createNewMovie = (e) => {
   e.preventDefault();
   const newEvent = {
-    eventName: document.getElementById('name').value,
+    name: document.getElementById('name').value,
     imageUrl: document.getElementById('image').value,
-    eventDate: document.getElementById('rating').value,
-    eventLocation: document.getElementById('date').value,
+    rating: document.getElementById('rating').value,
+    date: document.getElementById('date').value,
     uid: firebase.auth().currentUser.uid,
   };
   moviesData.addNewMovie(newEvent)
@@ -40,34 +40,54 @@ const addMovie = () => {
   }
 };
 
-const watchList = (e) => {
+const addToWatch = (e) => {
   const getCurrentId = e.target.closest('.eventCard').id;
-  console.error(getCurrentId);
-  document.getElementById('addWatchList').innerHTML.value('remove movie from watch list');
+  console.error(getCurrentId, 'test');
+  const updatedMovie = {
+    name: document.getElementById(`${getCurrentId}-name`).textContent,
+    imageUrl: document.getElementById(`${getCurrentId}-url`).src,
+    rating: document.getElementById(`${getCurrentId}-rating`).textContent,
+    date: document.getElementById(`${getCurrentId}-date`).textContent,
+    uid: firebase.auth().currentUser.uid,
+    watched: true,
+  };
+  console.error(updatedMovie);
+  moviesData.editMovie(getCurrentId, updatedMovie)
+    .then(() => {
+      getMovies(firebase.auth().currentUser.uid);// eslint-disable-line no-use-before-define
+    });
+};
+
+const watchList = () => {
+  const watchButton = document.getElementsByClassName('addWatchList');
+  for (let i = 0; i < watchButton.length; i += 1) {
+    watchButton[i].addEventListener('click', addToWatch);
+  }
 };
 
 const movieBuilder = (movies) => {
   let domString = ' ';
   movies.forEach((movie) => {
-    domString += `<div class="card eventCard m-2" id=${movie.id} style="width: 18rem;">`;
-    domString += `<h5 class="card-title">${movie.name}</h5>`;
-    domString += `<img class="card-img-top" id="event-pic" src="${movie.imageUrl}" alt="Card image cap" />`;
-    domString += `<p class="card-text">${movie.eventDate}</p>`;
-    domString += `<p class="card-text"> Date of Movie:${movie.date}</p>`;
+    domString += `<div class="card eventCard m-2" id="${movie.id}" style="width: 18rem;">`;
+    domString += `<h5 class="card-title" id="${movie.id}-name">${movie.name}</h5>`;
+    domString += `<img class="card-img-top" id="${movie.id}-url" src="${movie.imageUrl}" alt="Card image cap" />`;
+    domString += `<p class="card-text" id="${movie.id}-rating">${movie.rating}</p>`;
+    domString += `<p class="card-text" id="${movie.id}-date"> Date of Movie:${movie.date}</p>`;
     domString += '<button type="button" id="clicks" class="btn btn-light editButton">edit</button>';
     domString += '<button type="button" id="click" class="btn btn-light deleteButton">delete</button>';
-    domString += '<button type="button" id="addWatchList" class="btn btn-light eventCard">add movie to watch list</button>';
+    domString += `<button type="button" id="" class="btn btn-light addWatchList">${(movie.watched === true) ? 'remove from watch' : 'add to watch'}</button>`;
     domString += '</div>';
     // domString += '</div>';
   });
   util.printToDom('movie', domString);
   addMovie();
-  document.getElementById('addWatchList').addEventListener('click', watchList);
+  watchList();
 };
 
 const getMovies = (uid) => {
   moviesData.getMoviesByUid(uid)
     .then((movies) => {
+      console.error(movies);
       movieBuilder(movies);
     })
     .catch(err => console.error('no movies here', err));
